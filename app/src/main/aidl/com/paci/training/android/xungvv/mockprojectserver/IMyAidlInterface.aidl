8faaa7c29c25274e39e5b9f27e5d953d605747fb
Vu Van Xung <xungvu2k1@gmail.com>
2024-02-19 06:49:09 +0700

    16_2_2024

--- app/src/main/aidl/com/paci/training/android/xungvv/mockprojectserver/IMyAidlInterface.aidl
+++ app/src/main/aidl/com/paci/training/android/xungvv/mockprojectserver/IMyAidlInterface.aidl
@@ -1,8 +1,6 @@
 // IMyAidlInterface.aidl
 package com.paci.training.android.xungvv.mockprojectserver;
 
-// Declare any non-default types here with import statements
-
 interface IMyAidlInterface {
-    String getFruitDetail(String fruitName);
+    String getFruitDetail(int positionItem);
 }
--- app/src/main/java/com/paci/training/android/xungvv/mockprojectserver/FruitDetailService.java
+++ app/src/main/java/com/paci/training/android/xungvv/mockprojectserver/FruitDetailService.java
@@ -9,22 +9,22 @@ import com.paci.training.android.xungvv.mockprojectserver.data_local.FruitEntity
 import com.paci.training.android.xungvv.mockprojectserver.model.Fruit;
 
 public class FruitDetailService extends Service {
-    private final String TAG = "aidlService";
     public FruitDetailService() {
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        return new IMyAidlInterface.Stub() {
-            @Override
-            public String getFruitDetail(String fruitName) throws RemoteException {
-                for (Fruit fruit : FruitEntity.getFruits()){
-                    if(fruit.getName().equals(fruitName)){
-                        return fruit.getDetail();
-                    }
-                }
-                return "Invalid fruit name";
-            }
-        };
+        return binder;
     }
+    private final IMyAidlInterface.Stub binder = new IMyAidlInterface.Stub() {
+        @Override
+        public String getFruitDetail(int positionItem) throws RemoteException {
+            for (Fruit fruit : FruitEntity.getFruits()){
+                if(fruit.getId() == positionItem){
+                    return fruit.getDetail();
+                }
+            }
+            return "Invalid fruit id";
+        }
+    };
 }
